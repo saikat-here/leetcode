fbe8a4e3621008b1768453a1bb39b4458b19c5de
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2019-12-13 00:33:12 +0530

    Update Median of Two Sorted Arrays.cpp

--- solutions/Median of Two Sorted Arrays.cpp
+++ solutions/Median of Two Sorted Arrays.cpp
@@ -10,31 +10,40 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
     {
-
+        // Declaring a vector with size m+n
         vector <int> nums3((nums1.size()+nums2.size()),-1);
         int i=0,j=0,k=0;
         
+        // Any one input vector can be empty. If any one is empty then merge is not required. 
         if ((nums1.size()!=0) && (nums2.size()!=0))
         {
-        for (;k<=(nums1.size()+nums2.size());)
-        {
-            
-            nums3[k++]=nums1[i]>=nums2[j] ? nums2[j++]:nums1[i++];
-            
-            if ((i==nums1.size()) || (j==nums2.size()))
-                break;
-        }}
+            for (;k<=(nums1.size()+nums2.size());)
+            {
+                nums3[k++]=nums1[i]>=nums2[j] ? nums2[j++]:nums1[i++];
+                if ((i==nums1.size()) || (j==nums2.size())) // Vector size may not be equal. It will break when one vector reached to its end.
+                    break; 
+            }
+        }
         
+        //Now, it will migrate the positions of the vector that NOT reached to its end.
+        
+        // If nums1 NOT reached to its end
         while (i<(nums1.size()+0))
         {
             nums3[k++]=nums1[i++];
         }
 
+        // If nums2 NOT reached to its end
         while (j<(nums2.size()+0))
         {
             nums3[k++]=nums2[j++];
         }
+        /*
+          If the size of marged vector (nums3) is odd then it will have a Median position. Median possition will be (size of num3)/2
+          If the size of marged vector (num3) is even then there will be NO Median position, need to find the average of 2 middle possition.
+          So, the average value will be (nums3[nums3.size()/2]+nums3[(nums3.size()/2)-1])/2).
+                    Example : [3,4,5,6] => Need tind average of 4 and 5. 
+        */
         return nums3.size()%2==0 ? ((double)(nums3[nums3.size()/2]+nums3[(nums3.size()/2)-1])/2) : nums3[(int)nums3.size()/2];    
     }
-    
 };
