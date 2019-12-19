effcee1bfeeeb38f2d005c52c10f37b5cb6ad594
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2019-12-19 22:02:32 +0530

    Create Reverse Integer.cpp

---
+++ solutions/Reverse Integer.cpp
@@ -0,0 +1,23 @@
+/*
+    LeetCode Problem : Given a 32-bit signed integer, reverse digits of an integer.
+    Copyright (c) Saikat Sinha Ray
+*/
+
+
+class Solution {
+public:
+    int reverse(int x) 
+    {
+        int lastNum;
+        long long int newNum=0;
+
+          while (x !=0 )
+          {
+              newNum= (newNum*10)+(x%10);
+              if( newNum > 2147483647 || newNum < -2147483648)
+                return 0;
+              x=(int)x/10;
+         }
+        return newNum;
+    }
+};
