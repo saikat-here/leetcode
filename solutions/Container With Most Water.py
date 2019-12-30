d5fa22bd6dcecf66d612170538de3ccd7eff2db3
Saikat Sinha Ray <saikat.sinharay@gmail.com>
2019-12-31 00:45:56 +0530

    Create Container With Most Water.py

---
+++ solutions/Container With Most Water.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        endPoint=len(height)-1
+        start=capacity=max_capacity=0
+        
+        while start<endPoint:
+            
+            
+            if height[start] < height[endPoint]:
+                max_capacity= max_capacity if max_capacity > (height[start] * (endPoint-start)) else (height[start] * (endPoint-start))
+                start=start+1
+            else:
+                max_capacity= max_capacity if max_capacity > (height[endPoint]* (endPoint-start)) else (height[endPoint]* (endPoint-start))
+                endPoint=endPoint-1
+            
+
+        return max_capacity
+    
